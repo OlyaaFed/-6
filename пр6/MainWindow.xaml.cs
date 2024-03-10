b811faf7f28e9af7ec30da6f8a55657491cf0848
qwixryl <115480389+OlyaaFed@users.noreply.github.com>
2024-03-10 23:54:44 +0300

    Add files via upload

---
+++ пр6/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace пр6
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// выполнение основного алгоритма
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnResult_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int index = LstInput.SelectedIndex;
+                string str = (string)LstInput.Items[index];
+                List<int> list = new List<int>();
+                list.AddRange(str.Split(' ').Select(int.Parse));
+                int count = 0;
+                int i = 0;
+                while (i < list.Count)
+                {
+                    if (list[i] % 2 == 0)
+                    {
+                        count++;
+                    }
+                    i++;
+                }
+                CountOfEvenNumbers.Text = "Количество четных чисел = " + count.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void BtnLstFromFle_Click(object sender, RoutedEventArgs e)
+        {
+            LstInput.Items.Clear();
+            StreamReader sr = new StreamReader(@"файл.txt", Encoding.UTF8);
+
+            while (!sr.EndOfStream)
+            {
+                LstInput.Items.Add(sr.ReadLine());
+            }
+        }
+    }
+}
